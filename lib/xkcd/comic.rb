e6c37bfb507eb3f1d1974f8ed073ed1bdfe7938e
Fuad Saud <fuadksd@gmail.com>
2013-03-02 00:02:09 -0300

    Delegate JSON parsing to HTTParty

--- lib/xkcd/comic.rb
+++ lib/xkcd/comic.rb
@@ -5,7 +5,11 @@ module Xkcd
   class Comic
     class InvalidComicId < Exception; end
 
-    BASE_URI = 'http://xkcd.com'
+    include HTTParty
+
+    format :json
+    base_uri 'http://xkcd.com'
+
     JSON_PATH = '/info.0.json'
 
     attr_reader :id, :title, :alt, :img, :url, :date
@@ -14,16 +18,20 @@ module Xkcd
     def initialize(id = nil)
       if id == 404
         @id = 404
-        @title = 'Not found'
+        @title = 'Not Found'
       else
-        data = Comic.get(id ? "/#{id}#{JSON_PATH}" : JSON_PATH)
+        response = Comic.get(id ? "/#{id}#{JSON_PATH}" : JSON_PATH)
 
-        @id    = data[:num]
-        @title = data[:title]
-        @alt   = data[:alt]
-        @img   = data[:img]
-        @url   = "#{BASE_URI}/#{@id}"
-        @date  = Date.parse("#{data[:year]}-#{data[:month]}-#{data[:day]}")
+        raise InvalidComicId if response.code == 404
+
+        @id    = response['num']
+        @title = response['title']
+        @alt   = response['alt']
+        @img   = response['img']
+        @url   = "http://xkcd.com/#{@id}"
+        @date  = Date.parse(
+          "#{response['year']}-#{response['month']}-#{response['day']}"
+        )
       end
     end
 
@@ -31,17 +39,6 @@ module Xkcd
       def all
         last = Comic.new
         (1...last.id).each {|id| Comic.new(id) } << last
-      end
-
-      def get(path)
-        response = HTTParty.get(BASE_URI + path)
-
-        # 404 is not handled
-        begin
-          JSON.parse(response, { symbolize_names: true })
-        rescue Exception
-          raise InvalidComicId
-        end
       end
     end
   end
